0dc4b4339d8457f4af24c9b2c19a425db79e31fb
Nguyen Van Dat <datsunbae@gmail.com>
2022-10-08 16:38:17 +0700

    Post detail UI

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -5,77 +5,77 @@ const Home = () => {
   const posts = [
     {
       id: 1,
-      title: "HƯỚNG DẪN CHI TIẾT CÁCH PUSH CODE LÊN GIT",
+      title: "NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN",
       des: "Có lẽ các bạn theo đuổi lĩnh vực IT đã quen thuộc với Git và GitHub rồi đúng không nhỉ?",
       author: "Datsunbae",
       date: "November 25, 2022",
-      img: "https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/11/25171539/cach-push-code-len-git-1.png"
+      img: "https://cdn.dribbble.com/userupload/2817657/file/original-4c249c180db831f9bcc24f4bf01ee5b7.jpg?compress=1&resize=1024x1024"
     },
     {
       id: 2,
-      title: "HƯỚNG DẪN CHI TIẾT CÁCH PUSH CODE LÊN GIT",
+      title: "NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN",
       des: "Có lẽ các bạn theo đuổi lĩnh vực IT đã quen thuộc với Git và GitHub rồi đúng không nhỉ?",
       author: "Datsunbae",
       date: "November 25, 2022",
-      img: "https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/11/25171539/cach-push-code-len-git-1.png"
+      img: "https://cdn.dribbble.com/userupload/3678806/file/original-f5423389f689010ae6a6d1a08ef67983.png?compress=1&resize=1024x768"
     },
     {
       id: 3,
-      title: "HƯỚNG DẪN CHI TIẾT CÁCH PUSH CODE LÊN GIT",
+      title: "NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN",
       des: "Có lẽ các bạn theo đuổi lĩnh vực IT đã quen thuộc với Git và GitHub rồi đúng không nhỉ?",
       author: "Datsunbae",
       date: "November 25, 2022",
-      img: "https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/11/25171539/cach-push-code-len-git-1.png"
+      img: "https://cdn.dribbble.com/userupload/3368405/file/original-6bb6ea3370a5e1510940331d3175db67.png?compress=1&resize=1024x768"
     },{
       id: 4,
-      title: "HƯỚNG DẪN CHI TIẾT CÁCH PUSH CODE LÊN GIT",
+      title: "NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN",
       des: "Có lẽ các bạn theo đuổi lĩnh vực IT đã quen thuộc với Git và GitHub rồi đúng không nhỉ?",
       author: "Datsunbae",
       date: "November 25, 2022",
-      img: "https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/11/25171539/cach-push-code-len-git-1.png"
+      img: "https://cdn.dribbble.com/userupload/3267051/file/original-d63714084aae10607eb5322147062199.png?compress=1&resize=1024x768"
     }
     ,{
       id: 5,
-      title: "HƯỚNG DẪN CHI TIẾT CÁCH PUSH CODE LÊN GIT",
+      title: "NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN",
       des: "Có lẽ các bạn theo đuổi lĩnh vực IT đã quen thuộc với Git và GitHub rồi đúng không nhỉ?",
       author: "Datsunbae",
       date: "November 25, 2022",
-      img: "https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/11/25171539/cach-push-code-len-git-1.png"
+      img: "https://cdn.dribbble.com/userupload/3247623/file/original-666afac47856a95f3309d049e66d4677.jpg?compress=1&resize=1024x768"
     }
     ,{
       id: 6,
-      title: "HƯỚNG DẪN CHI TIẾT CÁCH PUSH CODE LÊN GIT",
+      title: "NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN",
       des: "Có lẽ các bạn theo đuổi lĩnh vực IT đã quen thuộc với Git và GitHub rồi đúng không nhỉ?",
       author: "Datsunbae",
       date: "November 25, 2022",
-      img: "https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/11/25171539/cach-push-code-len-git-1.png"
+      img: "https://cdn.dribbble.com/userupload/3186874/file/original-7372b05f3c9aef47a25b9fc805fe190d.png?compress=1&resize=1024x768"
     }
     ,{
       id: 7,
-      title: "HƯỚNG DẪN CHI TIẾT CÁCH PUSH CODE LÊN GIT",
+      title: "NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN",
       des: "Có lẽ các bạn theo đuổi lĩnh vực IT đã quen thuộc với Git và GitHub rồi đúng không nhỉ?",
       author: "Datsunbae",
       date: "November 25, 2022",
-      img: "https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/11/25171539/cach-push-code-len-git-1.png"
+      img: "https://cdn.dribbble.com/userupload/3106115/file/original-7ca08e77778233b2748808042d31e187.png?compress=1&resize=1024x768"
     }
     ,{
       id: 8,
-      title: "HƯỚNG DẪN CHI TIẾT CÁCH PUSH CODE LÊN GIT",
+      title: "NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN",
       des: "Có lẽ các bạn theo đuổi lĩnh vực IT đã quen thuộc với Git và GitHub rồi đúng không nhỉ?",
       author: "Datsunbae",
       date: "November 25, 2022",
-      img: "https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/11/25171539/cach-push-code-len-git-1.png"
+      img: "https://cdn.dribbble.com/userupload/2936601/file/original-2b2ba50bd8504aa031fedbf750bf3f70.jpg?compress=1&resize=1024x783"
     }
   ]
   return (
     <div className="home">
     <div className="new-post">
       <div className="new-post__img">
-        <img src="https://d1iv5z3ivlqga1.cloudfront.net/wp-content/uploads/2021/11/25171539/cach-push-code-len-git-1.png" alt=""/>
+        <img src="https://cdn.dribbble.com/userupload/2936601/file/original-2b2ba50bd8504aa031fedbf750bf3f70.jpg?compress=1&resize=1024x783" alt=""/>
       </div>
       <div className="post-info">
         <span classNanme="new-post__date">November 25, 2022</span>
-        <h1 className="new-post__title">HƯỚNG DẪN CHI TIẾT CÁCH PUSH CODE LÊN GIT</h1>
+        <h1 className="new-post__title">NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN</h1>
         <p className="new-post__des">Có lẽ các bạn theo đuổi lĩnh vực IT đã quen thuộc với Git và GitHub rồi đúng không nhỉ?</p>
         <span className="new-post__author">Datsunbae</span>
       </div>
--- client/src/pages/Post/index.jsx
+++ client/src/pages/Post/index.jsx
@@ -1,9 +1,208 @@
-import React from 'react'
+import React from "react";
+import "./Post.scss";
 
 const Post = () => {
   return (
-    <div>Post</div>
-  )
-}
+    <div className="post-page">
+      <div className="post-detail">
+        <div className="post-detail__img">
+          <img
+            src="https://cdn.dribbble.com/userupload/3639911/file/original-ae545cef43334b009486d28c3471ce09.png?compress=1&resize=1024x768"
+            alt=""
+          />
+        </div>
+        <div className="post-detail__content">
+          <div className="post-detail__author-wrapper">
+            <img
+              src="https://cdn.dribbble.com/users/3562886/avatars/normal/6ea75a78a4f4baa3d9905b7e4e1438ef.jpg?1664044951"
+              alt=""
+            />
+            <div className="post-detail__author">
+              <h4 className="post-detail__author-name">Datsunbae</h4>
+              <span className="post-detail__author-date">
+                Posted an hour ago
+              </span>
+            </div>
+          </div>
+          <h1 className="post-detail__title">Can it leap tall buildings in a single bound or protect the rights of the innocent? </h1>
+          <p className="post-detail__content-post">
+            What makes a supercomputer so super? Can it leap tall buildings in a
+            single bound or protect the rights of the innocent? The truth is a
+            bit more mundane. Supercomputers can process complex calculations
+            very quickly. As it turns out, that's the secret behind computing
+            power. It all comes down to how fast a machine can perform an
+            operation. Everything a computer does breaks down into math. Your
+            computer's processor interprets any command you execute as a series
+            of math problems. Faster processors can handle more calculations per
+            second than slower ones, and they're also better at handling really
+            tough calculations. Within your computer's CPU is an electronic
+            clock. The clock's job is to create a series of electrical pulses at
+            regular intervals. This allows the computer to synchronize all its
+            components and it determines the speed at which the computer can
+            pull data from its memory and perform calculations. When you talk
+            about how many gigahertz your processor has, you're really talking
+            about clock speed. The number refers to how many electrical pulses
+            your CPU sends out each second. A 3.2 gigahertz processor sends out
+            around 3.2 billion pulses each second. While it's possible to push
+            some processors to speeds faster than their advertised limits -- a
+            process called overclocking -- eventually a clock will hit its limit
+            and will go no faster. As of March 2010, the record for processing
+            power goes to a Cray XT5 computer called Jaguar. The Jaguar
+            supercomputer can process up to 2.3 quadrillion calculations per
+            second [source: National Center for Computational Sciences].
+            Computer performance can also be measured in floating-point
+            operations per second, or flops. Current desktop computers have
+            processors that can handle billions of floating-point operations per
+            second, or gigaflops. Computers with multiple processors have an
+            advantage over single-processor machines, because each processor
+            core can handle a certain number of calculations per second.
+            Multiple-core processors increase computing power while using less
+            electricity [source: Intel] Even fast computers can take years to
+            complete certain tasks. Finding two prime factors of a very large
+            number is a difficult task for most computers. First, the computer
+            must determine the factors of the large number. Then, the computer
+            must determine if the factors are prime numbers. For incredibly
+            large numbers, this is a laborious task. The calculations can take a
+            computer many years to complete. Future computers may find such a
+            task relatively simple. A working quantum computer of sufficient
+            power could calculate factors in parallel and then provide the most
+            likely answer in just a few moments. However, quantum computers have
+            their own challenges and wouldn't be suitable for all computing
+            tasks, but they could reshape the way we think of computing power.
+            Learn more about computers and processors by following the links on
+            the next page.
+            What makes a supercomputer so super? Can it leap tall buildings in a
+            single bound or protect the rights of the innocent? The truth is a
+            bit more mundane. Supercomputers can process complex calculations
+            very quickly. As it turns out, that's the secret behind computing
+            power. It all comes down to how fast a machine can perform an
+            operation. Everything a computer does breaks down into math. Your
+            computer's processor interprets any command you execute as a series
+            of math problems. Faster processors can handle more calculations per
+            second than slower ones, and they're also better at handling really
+            tough calculations. Within your computer's CPU is an electronic
+            clock. The clock's job is to create a series of electrical pulses at
+            regular intervals. This allows the computer to synchronize all its
+            components and it determines the speed at which the computer can
+            pull data from its memory and perform calculations. When you talk
+            about how many gigahertz your processor has, you're really talking
+            about clock speed. The number refers to how many electrical pulses
+            your CPU sends out each second. A 3.2 gigahertz processor sends out
+            around 3.2 billion pulses each second. While it's possible to push
+            some processors to speeds faster than their advertised limits -- a
+            process called overclocking -- eventually a clock will hit its limit
+            and will go no faster. As of March 2010, the record for processing
+            power goes to a Cray XT5 computer called Jaguar. The Jaguar
+            supercomputer can process up to 2.3 quadrillion calculations per
+            second [source: National Center for Computational Sciences].
+            Computer performance can also be measured in floating-point
+            operations per second, or flops. Current desktop computers have
+            processors that can handle billions of floating-point operations per
+            second, or gigaflops. Computers with multiple processors have an
+            advantage over single-processor machines, because each processor
+            core can handle a certain number of calculations per second.
+            Multiple-core processors increase computing power while using less
+            electricity [source: Intel] Even fast computers can take years to
+            complete certain tasks. Finding two prime factors of a very large
+            number is a difficult task for most computers. First, the computer
+            must determine the factors of the large number. Then, the computer
+            must determine if the factors are prime numbers. For incredibly
+            large numbers, this is a laborious task. The calculations can take a
+            computer many years to complete. Future computers may find such a
+            task relatively simple. A working quantum computer of sufficient
+            power could calculate factors in parallel and then provide the most
+            likely answer in just a few moments. However, quantum computers have
+            their own challenges and wouldn't be suitable for all computing
+            tasks, but they could reshape the way we think of computing power.
+            Learn more about computers and processors by following the links on
+            the next page.
+            What makes a supercomputer so super? Can it leap tall buildings in a
+            single bound or protect the rights of the innocent? The truth is a
+            bit more mundane. Supercomputers can process complex calculations
+            very quickly. As it turns out, that's the secret behind computing
+            power. It all comes down to how fast a machine can perform an
+            operation. Everything a computer does breaks down into math. Your
+            computer's processor interprets any command you execute as a series
+            of math problems. Faster processors can handle more calculations per
+            second than slower ones, and they're also better at handling really
+            tough calculations. Within your computer's CPU is an electronic
+            clock. The clock's job is to create a series of electrical pulses at
+            regular intervals. This allows the computer to synchronize all its
+            components and it determines the speed at which the computer can
+            pull data from its memory and perform calculations. When you talk
+            about how many gigahertz your processor has, you're really talking
+            about clock speed. The number refers to how many electrical pulses
+            your CPU sends out each second. A 3.2 gigahertz processor sends out
+            around 3.2 billion pulses each second. While it's possible to push
+            some processors to speeds faster than their advertised limits -- a
+            process called overclocking -- eventually a clock will hit its limit
+            and will go no faster. As of March 2010, the record for processing
+            power goes to a Cray XT5 computer called Jaguar. The Jaguar
+            supercomputer can process up to 2.3 quadrillion calculations per
+            second [source: National Center for Computational Sciences].
+            Computer performance can also be measured in floating-point
+            operations per second, or flops. Current desktop computers have
+            processors that can handle billions of floating-point operations per
+            second, or gigaflops. Computers with multiple processors have an
+            advantage over single-processor machines, because each processor
+            core can handle a certain number of calculations per second.
+            Multiple-core processors increase computing power while using less
+            electricity [source: Intel] Even fast computers can take years to
+            complete certain tasks. Finding two prime factors of a very large
+            number is a difficult task for most computers. First, the computer
+            must determine the factors of the large number. Then, the computer
+            must determine if the factors are prime numbers. For incredibly
+            large numbers, this is a laborious task. The calculations can take a
+            computer many years to complete. Future computers may find such a
+            task relatively simple. A working quantum computer of sufficient
+            power could calculate factors in parallel and then provide the most
+            likely answer in just a few moments. However, quantum computers have
+            their own challenges and wouldn't be suitable for all computing
+            tasks, but they could reshape the way we think of computing power.
+            Learn more about computers and processors by following the links on
+            the next page.
+          </p>
+        </div>
+      </div>
+      <div className="other-post">
+        <div className="other-post__title">
+          Other posts you may like
+        </div>
+        <ul className="other-post__list">
+          <li className="other-post__item">
+              <img src="https://cdn.dribbble.com/users/3562886/screenshots/14761289/media/7ef51c4dec10d815ea68c3c95f2b88c8.png?compress=1&resize=1000x750&vertical=top" alt=""/>
+              <h3 className="other-post__item-title">NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN</h3>
+              <button className="other-post__btn">Read More</button>
+          </li>
+          <li className="other-post__item">
+              <img src="https://cdn.dribbble.com/users/3562886/screenshots/14761289/media/7ef51c4dec10d815ea68c3c95f2b88c8.png?compress=1&resize=1000x750&vertical=top" alt=""/>
+              <h3 className="other-post__item-title">NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN</h3>
+              <button className="other-post__btn">Read More</button>
+          </li>
+          <li className="other-post__item">
+              <img src="https://cdn.dribbble.com/users/3562886/screenshots/14761289/media/7ef51c4dec10d815ea68c3c95f2b88c8.png?compress=1&resize=1000x750&vertical=top" alt=""/>
+              <h3 className="other-post__item-title">NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN</h3>
+              <button className="other-post__btn">Read More</button>
+          </li>
+          <li className="other-post__item">
+              <img src="https://cdn.dribbble.com/users/3562886/screenshots/14761289/media/7ef51c4dec10d815ea68c3c95f2b88c8.png?compress=1&resize=1000x750&vertical=top" alt=""/>
+              <h3 className="other-post__item-title">NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN</h3>
+              <button className="other-post__btn">Read More</button>
+          </li>
+          <li className="other-post__item">
+              <img src="https://cdn.dribbble.com/users/3562886/screenshots/14761289/media/7ef51c4dec10d815ea68c3c95f2b88c8.png?compress=1&resize=1000x750&vertical=top" alt=""/>
+              <h3 className="other-post__item-title">NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN</h3>
+              <button className="other-post__btn">Read More</button>
+          </li>
+          <li className="other-post__item">
+              <img src="https://cdn.dribbble.com/users/3562886/screenshots/14761289/media/7ef51c4dec10d815ea68c3c95f2b88c8.png?compress=1&resize=1000x750&vertical=top" alt=""/>
+              <h3 className="other-post__item-title">NORTH KOREA FIRES BALLISTIC MISSILE OVER JAPAN</h3>
+              <button className="other-post__btn">Read More</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Post
+export default Post;
