a953c644e61c7f14ac8d38ea576479d0cbdcd88a
Nguyen Van Dat <datsunbae@gmail.com>
2023-03-19 09:47:11 +0700

    Integrated cloudinary

--- client/src/pages/Write/index.jsx
+++ client/src/pages/Write/index.jsx
@@ -1,25 +1,42 @@
-import React, {useState} from 'react';
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import './Write.scss';
+import axios from "axios";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import "./Write.scss";
 
 const Write = () => {
   const [value, setValue] = useState();
-  const [image, setImage] = useState(null);
-  const onImageChange = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      setImage(URL.createObjectURL(event.target.files[0]));
+  const [image, setImage] = useState("");
+  const onImageChange = async (img) => {
+    if (img.type === "image/jpeg" || img.type === "image/png") {
+      const data = new FormData();
+      data.append("file", img);
+      data.append("upload_preset", process.env.REACT_APP_UPLOAD_PRESET);
+      data.append("cloud_name", process.env.REACT_APP_CLOUD_NAME);
+
+      try {
+        const res = await axios.post(
+          process.env.REACT_APP_URL_API_CLOUDINARY,
+          data
+        );
+
+        setImage(res.data.url.toString());
+      } catch (err) {}
     }
-   }
-  console.log(value);
+  };
   return (
     <div className="write">
       <div className="write-row-1">
         <div className="write-title">
-          <input type="text" placeholder="Enter title post ..."/>
+          <input type="text" placeholder="Enter title post ..." />
         </div>
         <div className="write__form-content">
-          <ReactQuill className="write__form-content-editor" theme="snow" value={value} onChange={setValue}/>
+          <ReactQuill
+            className="write__form-content-editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
         </div>
         <button className="btn-post">POST</button>
       </div>
@@ -35,41 +52,54 @@ const Write = () => {
           </div>
           <div className="info-post__thumb">
             <h4>Thumbnail:</h4>
-            <input type="file" onChange={onImageChange} className="filetype" />
-            <img src={image} alt=""/>
+            <input
+              type="file"
+              onChange={(e) => onImageChange(e.target.files[0])}
+              className="filetype"
+            />
+            <img src={image} alt="" />
           </div>
           <div className="info-post__category">
             <h4>Category:</h4>
             <div className="info-post__category-item">
-              <input name="category" id="art" value="art" type="radio"/>
+              <input name="category" id="art" value="art" type="radio" />
               <label htmlFor="art">Art</label>
             </div>
             <div className="info-post__category-item">
-              <input name="category" id="science" value="science" type="radio"/>
+              <input
+                name="category"
+                id="science"
+                value="science"
+                type="radio"
+              />
               <label htmlFor="science">Science</label>
             </div>
             <div className="info-post__category-item">
-              <input name="category" id="teachnology" value="teachnology" type="radio"/>
+              <input
+                name="category"
+                id="teachnology"
+                value="teachnology"
+                type="radio"
+              />
               <label htmlFor="teachnology">Teachnology</label>
             </div>
             <div className="info-post__category-item">
-              <input name="category" id="cinema" value="cinema" type="radio"/>
+              <input name="category" id="cinema" value="cinema" type="radio" />
               <label htmlFor="cinema">Cinema</label>
             </div>
             <div className="info-post__category-item">
-              <input name="category" id="design" value="design" type="radio"/>
+              <input name="category" id="design" value="design" type="radio" />
               <label htmlFor="design">Design</label>
             </div>
             <div className="info-post__category-item">
-              <input name="category" id="food" value="food" type="radio"/>
+              <input name="category" id="food" value="food" type="radio" />
               <label htmlFor="food">Food</label>
             </div>
           </div>
         </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Write
+export default Write;
